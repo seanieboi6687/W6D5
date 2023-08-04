8466c85f64fa0a28bfbed44159f2c5675fcb847c
Sean <pca.sean.jeun@gmail.com>
2023-08-04 17:00:40 -0400

    usercontroller build complete

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/controllers/cats_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -5,18 +5,26 @@ class CatsController < ActionController::Base
     end
 
     def create
-        
+        cat = Cat.new(self.cat_params)
+        if cat.save
+            render json: cat
+        else
+            render json: cat.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-
+        cat = Cat.find(params[:id])
+        render json: cat
     end
 
     def update
-
+        cat = Cat.find(params[:id])
+        cat.update(self.cat_params)
+        render json: cat
     end
 
-     def cats_params
+     def cat_params
         params.require(:cat).permit(:name)
     end
 end
