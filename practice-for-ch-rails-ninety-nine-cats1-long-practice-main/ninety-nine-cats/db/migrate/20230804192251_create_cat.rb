7f31968e0379e3be705f7b2441174f2ce73d53cb
Sean <pca.sean.jeun@gmail.com>
2023-08-04 15:40:46 -0400

    db migrated/model created

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/db/migrate/20230804192251_create_cat.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/db/migrate/20230804192251_create_cat.rb
@@ -1,7 +1,11 @@
 class CreateCat < ActiveRecord::Migration[7.0]
   def change
     create_table :cats do |t|
-      
+      t.string :name, null: false
+      t.string  :color, null: false
+      t.integer :birth_date, null: false
+      t.string :sex, null: false, limit: 1
+      t.text :description
       t.timestamps
     end
   end
